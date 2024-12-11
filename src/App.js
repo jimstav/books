ad0141e79b03b2aab9bed4081dbbaccc43e356b1
Dimitris Stavroulakis <jimstav@gmail.com>
2024-12-11 17:50:14 +0200

    Updating the Title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   const deleteBookById = (id) => {
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
@@ -22,7 +33,7 @@ function App() {
 
   return (
     <div className="app">
-      <BookList books={books} onDelete={deleteBookById} />
+      <BookList books={books} onEdit={editBookById} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
