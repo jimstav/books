b225a8778db1e9d0faf3f9ab4e8462413f86db97
Dimitris Stavroulakis <jimstav@gmail.com>
2024-12-21 14:02:16 +0200

    Context in Action

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import BooksContext from "./context/books";
 import "./index.css";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <BooksContext.Provider value={10}>
+    <App />
+  </BooksContext.Provider>
+);
